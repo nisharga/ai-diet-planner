436df07843c88cc868e2c1deee3cc5d64417a7b4
nisharga <kabirnisharga@gmail.com>
2025-08-22 22:23:41 +0600

    feat: convix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,20 @@
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="index" />
-    </Stack>
+    <ConvexProvider client={convex}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="index" />
+      </Stack>
+    </ConvexProvider>
   );
 }
---
+++ convex/schema.ts
@@ -0,0 +1,37 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  users: defineTable({
+    name: v.string(),
+    email: v.string(),
+    picture: v.optional(v.string()),
+    subscriptionId: v.optional(v.string()),
+    credits: v.number(),
+  
+  }),
+
+
+    /* height: v.optional(v.string()),
+    weight: v.optional(v.string()),
+    age: v.optional(v.string()),
+    gender: v.optional(v.string()),
+    goal: v.optional(v.string()),
+    calories: v.optional(v.number()),
+    proteins: v.optional(v.number()), */
+//   recipes: defineTable({
+//     jsonData: v.any(),
+//     imageUrl : v.optional(v.string()),
+//     uid: v.id("users"),
+//     recipeName : v.string(),
+//   }),
+//   mealPlan:defineTable({
+//     recipeId:v.id('recipes'),
+//     date:v.string(),
+//     mealType:v.string(),
+//     uid:v.id("users"),
+//     status: v.optional(v.boolean()),
+//     calories: v.optional(v.number()),
+//   })
+});
+
