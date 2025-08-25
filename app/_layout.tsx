aeab6e5b49ea60f658e085528c2c99ad91e5a45f
nisharga <kabirnisharga@gmail.com>
2025-08-25 19:23:45 +0600

    feat: google-sign-in added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,8 @@
 import { UserContext } from "@/context/UserContent";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
   unsavedChangesWarning: false,
@@ -9,6 +10,16 @@ const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
 
 export default function RootLayout() {
   const [user, setUser] = useState<any>();
+  useEffect(() => {
+    GoogleSignin.configure({
+      iosClientId:
+        "74246080296-1l2mhq80nir1q5u8uieio7j25i3hdocm.apps.googleusercontent.com",
+      webClientId:
+        "74246080296-cj1u9k8tnh7nmucpc1o17hqisid5916b.apps.googleusercontent.com",
+      profileImageSize: 150,
+    });
+  });
+
   return (
     <ConvexProvider client={convex}>
       <UserContext.Provider value={[user, setUser]}>
--- app/auth/SignIn.tsx
+++ app/auth/SignIn.tsx
@@ -1,12 +1,46 @@
 import Button from "@/components/shared/Button";
 import Input from "@/components/shared/Input";
+import { UserContext } from "@/context/UserContent";
+import { api } from "@/convex/_generated/api";
+import { auth } from "@/services/FirebaseConfig";
+import {
+  GoogleSignin,
+  isSuccessResponse,
+} from "@react-native-google-signin/google-signin";
+import { useConvex } from "convex/react";
 import { Link } from "expo-router";
-import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useContext, useState } from "react";
 import { Alert, Image, Text, View } from "react-native";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const convex = useConvex();
+  const [user, setUser] = useContext(UserContext) as any;
+
+  //   google login
+  const handleGoolgeSignIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const response = await GoogleSignin.signIn();
+      if (isSuccessResponse(response)) {
+        const user = {
+          email: response?.data?.user?.email,
+          name: response?.data?.user?.name,
+          credits: 10,
+        };
+        setUser(user);
+        if (user) {
+          Alert.alert("User Login Successfully!!");
+        }
+      } else {
+        // sign in was cancelled by user
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   //   handle login
   const onSignIn = () => {
@@ -14,7 +48,24 @@ const SignIn = () => {
       Alert.alert("Missing Fields", "Please fill all the values!!");
       return;
     }
-    console.log("email", email);
+    signInWithEmailAndPassword(auth, email, password)
+      .then(async (userCredential) => {
+        const userId = userCredential.user;
+        console.log("🚀 ~ onSignIn ~ userId:", userId);
+        const userData = await convex.query(api.User.GetUser, {
+          email: email,
+        });
+        setUser(userData);
+        if (user) {
+          Alert.alert("User Login Successfully!!");
+        }
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        console.log("🚀 ~ onSignIn ~ errorCode:", errorCode);
+        const errorMessage = error.message;
+        Alert.alert("Invalid Email or Password", errorMessage);
+      });
   };
 
   return (
@@ -53,6 +104,10 @@ const SignIn = () => {
         />
         <View style={{ width: "100%", marginTop: 20 }}>
           <Button title="Sign In" onPress={() => onSignIn()} />
+        </View>
+
+        <View style={{ width: "100%", marginTop: 20 }}>
+          <Button title="Google Sign In" onPress={() => handleGoolgeSignIn()} />
         </View>
 
         {/* other */}
